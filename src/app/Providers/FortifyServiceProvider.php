1e0c109aef8fd4722abe604332c830ead0721ea4
shizuka tomura <s.tomura724@gmail.com>
2024-11-20 15:23:13 +0900

    11/20 ログイン機能実装

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -30,7 +30,7 @@ class FortifyServiceProvider extends ServiceProvider
     {
         Fortify::createUsersUsing(CreateNewUser::class);
 
-        Fortify::registerView(function (Request $request) {
+        Fortify::registerView(function () {
             return view('auth.register');
         });
 
--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,16 +1,50 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/register.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/login.css') }}" />
 @endsection
 
 @section('content')
-ログインだよ
-<form class="form" action="/login" method="post">
-@csrf
-    メール<input type="email" name="email" value="{{ old('email') }}">
-    パスワード<input type="password" name="password">
-    <button>ログイン</button>
-</form>
+    <div class="content">
+        <h2 class="content__title">ログイン</h2>
 
+        <form class="form" action="/login" method="post">
+        @csrf
+            <div class="form__group">
+                <div class="form__group--title">
+                    <span class="form__label">ユーザー名/メールアドレス</span>
+                </div>
+                <div class="form__group--input">
+                    <input type="email" name="email" value="{{ old('email') }}">
+                </div>
+                <div class="form__error">
+                    @error('email')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form__group">
+                <div class="form__group--title">
+                    <span class="form__label">パスワード</span>
+                </div>
+                <div class="form__group--input">
+                    <input type="password" name="password">
+                </div>
+                <div class="form__error">
+                    @error('password')
+                    {{ $message }}
+                    @enderror
+                </div>
+            </div>
+            
+            <div class="form__button">
+                <button class="form__button--submit" type="submit">ログイン</button>
+            </div>
+        </form>
+
+        <div class="link__register">
+            <a href="/register">会員登録はこちら</a>
+        </div>
+    </div>
 @endsection
