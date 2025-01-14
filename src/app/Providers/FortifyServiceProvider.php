aa70e8258fdeeee8674803fcc1b1d4b1c9da8308
shizuka tomura <s.tomura724@gmail.com>
2025-01-14 18:41:46 +0900

    1/14 更新

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
-use App\Http\Requests\LoginRequest;
+use Laravel\Fortify\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 
 class FortifyServiceProvider extends ServiceProvider
